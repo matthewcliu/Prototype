bcc062b594c828abdeb50b69caffcca6da2ff167
Matthew Liu <matthewliu@gmail.com>
2013-06-10 17:33:17 -0700

    Initial Commit

---
+++ Prototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Prototype
+//
+//  Created by Matthew Liu on 6/10/13.
+//  Copyright (c) 2013 Matthew Liu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
